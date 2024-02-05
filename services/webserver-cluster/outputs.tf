0bbc0d9d4c82cbb2786046c5b145927c268c048d
Lukas Savukaitis <lukas.savukaitis@gmail.com>
2024-02-05 12:29:55 +0200

    comment out outputs.tf

--- services/webserver-cluster/outputs.tf
+++ services/webserver-cluster/outputs.tf
@@ -1,7 +1,7 @@
-output "alb_dns_name" {
-  value       = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
-}
+#output "alb_dns_name" {
+#  value       = aws_lb.example.dns_name
+#  description = "The domain name of the load balancer"
+#}
 
 output "asg_name" {
   value = aws_autoscaling_group.example.name
